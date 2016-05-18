aab3cda6d8bdff4e1013606a774ad3d7c853fae4
Mateus Pires <mateusplpl@gmail.com>
2016-05-18 03:51:46 -0300

    Test pass but result is invalid. List implementation needed

--- EDStore/src/ed/store/database/tests/DatabaseTest.java
+++ EDStore/src/ed/store/database/tests/DatabaseTest.java
@@ -1,86 +1,32 @@
 package ed.store.database.tests;
 
-import ed.store.database.KeyValuePair;
-import ed.store.database.enums.Conditions;
+import ed.store.database.StructuresDBMS;
 import ed.store.database.enums.Structures;
-import ed.store.database.interfaces.ColumnValidator;
-import ed.store.database.interfaces.Conditional;
+import ed.store.database.exceptions.DatabaseOverwriteException;
+import ed.store.database.exceptions.InvalidNameException;
+import ed.store.database.exceptions.NoPermissionException;
 import ed.store.database.interfaces.Database;
-import ed.store.database.interfaces.Query;
-import ed.store.database.interfaces.Relational;
-import ed.store.database.interfaces.Table;
+import ed.store.database.interfaces.Map;
 
 public class DatabaseTest {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws NoPermissionException, InvalidNameException
 	{
-		Database db = new StructuresDatabase();
-		
-		Table t = db.createTable("my_table");
-		
-		t.addColumn("name", null, new String[0], new ColumnValidator<String>() {
-				@Override
-				public boolean validate(String data) {
-					return data.length() >= 3 && data.matches("[a-zA-Z ]*");
-				}
-			});
-		t.addColumn("age", new DataStruct(1, 2), new DataStruct[0], new ColumnValidator<DataStruct>() {
-			@Override
-			public boolean validate(DataStruct data) {
-				return data.a != data.b;
-			}
-		});
-		t.addColumn("packet", null);
-		
-		t.insert(new KeyValuePair[] {
-				new KeyValuePair("name", "Mateus Pires"),
-				new KeyValuePair("age", "21"),
-				new KeyValuePair("packet", new DataStruct(2, 3))
-			});
-		t.insert(new KeyValuePair[] {
-				new KeyValuePair("name", "abc123"),
-				new KeyValuePair("age", "5"),
-				new KeyValuePair("packet", new DataStruct(2, 2))
-			});
-		
-		Query q = new BasicQuery();
-		
-		q.add("name", Conditions.GREATER, "A");
-		q.add("age", new Conditional<Integer>() {
-				@Override
-				public boolean check(Integer data) {
-					return data >= 18 && data <= 50;
-				}
-			});
-		q.add("packet", new Relational<DataStruct>() {
-				@Override
-				public boolean check(DataStruct data, DataStruct rel) {
-					return data.a < rel.a && data.b > rel.b;
-				}
-			}, null);
-		
-		Object[][] data = t.select(q);
-		
-		for (Object[] row : data)
-			for (Object col : row)
-				System.out.println(col);
-	}
-	
-	
-	private static class DataStruct implements Comparable<DataStruct> {
-		
-		public int a, b;
-		
-		public DataStruct(int a, int b) {
-			this.a = a;
-			this.b = b;
-		}
-
-		@Override
-		public int compareTo(DataStruct comp) {
-			return this.a + this.b - comp.a + comp.b;
+		StructuresDBMS dbms;
+		try {
+			dbms = StructuresDBMS.create();
+		} catch (DatabaseOverwriteException doe) {
+			dbms = StructuresDBMS.open();
 		}
 		
+		Database db = dbms.createDatabase("database");
+		
+		@SuppressWarnings("unchecked")
+		Map<Integer, String> ms = (Map<Integer, String>) db.create(Structures.MAP, "string_map", new Integer[0], new String[0]);
+		
+		ms.put(1, "Mateus Pires");
+		
+		System.out.println(ms.get(1));
 	}
 	
 }
