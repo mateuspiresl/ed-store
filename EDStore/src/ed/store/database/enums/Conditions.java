fee675871285f4087c3ef96593fa9d8ebd5e75c6
Mateus Pires <mateusplpl@gmail.com>
2016-05-26 03:04:11 -0300

    Adds method to Conditions to check the comparison's value

--- EDStore/src/ed/store/database/enums/Conditions.java
+++ EDStore/src/ed/store/database/enums/Conditions.java
@@ -3,5 +3,16 @@ package ed.store.database.enums;
 public enum Conditions {
 
 	EQUAL, GREATER, SMALLER;
+	
+	public boolean check(int compareResult)
+	{
+		switch (this)
+		{
+		case EQUAL:		return compareResult == 0;
+		case GREATER:	return compareResult > 0;
+		case SMALLER:	return compareResult < 0;
+		default:		return false;
+		}
+	}
 
 }
